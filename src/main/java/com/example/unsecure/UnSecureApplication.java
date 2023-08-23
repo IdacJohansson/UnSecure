3966d31604d65f5bf5c67f75fe0781742a7a5894
Ida Grufman Johansson <johansson.c.ida@gmail.com>
2023-08-23 13:26:47 +0200

    Initial commit

---
+++ src/main/java/com/example/unsecure/UnSecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.unsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnSecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnSecureApplication.class, args);
+    }
+
+}
